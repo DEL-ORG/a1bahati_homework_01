9824a9567cdedd7d51a0e71ef04a0431ddc8c5cb
Bahati <ntamabahati0316@gmail.com>
2024-11-03 18:57:19 -0500

    Homework #01

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "homework_01" {
+  ami           = "ami-0cde6390e61a03eee"  # Replace with an AMI ID of your choice
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-080ae30c3b2f0782f"        # Replace with the actual Subnet ID
+
+  tags = {
+    Name = "Homework_EC2Instance"
+  }
+}
+
